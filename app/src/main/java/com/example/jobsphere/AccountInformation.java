0ca2a0a24d69612198a01a5580a54b9172f545ff
MohamedShiiraar <mso2shiiraar@gmail.com>
2024-08-01 14:49:57 +0200

    Base done

--- app/src/main/java/com/example/jobsphere/AccountInformation.java
+++ app/src/main/java/com/example/jobsphere/AccountInformation.java
@@ -15,7 +15,7 @@ public class AccountInformation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_account_information);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btnActivity), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
