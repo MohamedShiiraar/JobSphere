1259e9316cf7de9992f41c4256f60eaa47553690
Gammaadmo <220208344@mycput.ac.za>
2024-08-03 15:24:17 +0200

    GammaadMohamed_User

--- app/src/main/java/com/example/jobsphere/model/User.java
+++ app/src/main/java/com/example/jobsphere/model/User.java
@@ -1,4 +1,47 @@
 package com.example.jobsphere.model;
 
 public class User {
-}
+
+        private String Title;
+        private String Company;
+        private String Location;
+        private double Salary;
+        private String Description;
+
+
+        public User(String Title, String Company, String Location, double Salary, String Description) {
+            this.Title = Title;
+            this.Company = Company;
+            this.Location = Location;
+            this.Salary = Salary;
+            this.Description = Description;
+        }
+
+
+        public String displayDetails() {
+            return String.format(
+                    "Job Title: %s\nCompany: %s\nLocation: %s\nSalary: $%.2f\nDescription: %s",
+                    Title, Company, Location, Salary, Description
+            ) ;
+
+        }
+
+
+        public String updateSalary(double newSalary) {
+            double oldSalary = this.Salary;
+            this.Salary = newSalary;
+            return String.format("salary has been updated from $%.2f to $%.2f.", oldSalary, newSalary);
+        }
+
+
+        @Override
+        public String toString() {
+            return String.format("%s at %s in %s ($%.2f)", Title, Company, Location, Salary);
+        }
+
+
+
+        }
+
+
+
